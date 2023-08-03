8553f219b3f5c5d7e9addbeb3d8a82a2dc6e7c09
Dena Alharbi <109204661+DenaAlharbi@users.noreply.github.com>
2023-08-03 13:24:05 +0300

    Add files via upload

--- Images - Exit - Icons.py
+++ Images - Exit - Icons.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from PIL import ImageTk, Image  # It is used to import images easily
 
 root = Tk()
 root.title("Images - Exit - Icons")
@@ -6,6 +7,9 @@ root.title("Images - Exit - Icons")
 photo = PhotoImage(file="download.png")
 root.iconphoto(False, photo)
 
+# Using images
+my_img = ImageTk.PhotoImage(Image.open("blahblah.png"))  # The PhotoImage is the same as before
+my_label = Label(image=my_img)
 # An exit button
 button_quit = Button(root, text="Exit Program", command=root.quit)
 button_quit.pack()
